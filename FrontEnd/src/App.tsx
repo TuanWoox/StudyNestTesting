9e1a1079e8c260f81e4729ea8e4e9d88c883623e
Vu Nguyen <z1venttt@gmail.com>
2025-08-26 08:39:12 +0700

    Add Toast and Redux

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,17 @@
 // App.tsx or main.tsx
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { router } from "./Router";
+import { Toaster } from 'sonner';
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster richColors expand={false} position="top-right" />
+    </Provider>
+  )
 }
 
 export default App;
---
+++ FrontEnd/src/config/axiosConfig.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosResponse } from "axios";
+import { ReturnResult } from "../types/common/return-result";
+import { toast } from "sonner";
+
+const defaultOptions = {
+    baseURL: import.meta.env.BASE_URL
+};
+
+const instance = axios.create(defaultOptions);
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('accessToken');
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+},
+
+);
+
+// Add a response interceptor, if the return result has message => that means we have error
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+instance.interceptors.response.use(function onFulfilled(response: AxiosResponse<ReturnResult<any>>) {
+    if (response.data.message) {
+        toast.error(response.data.message);
+    }
+    return response;
+}, function onRejected(error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+});
+
+export default instance;
