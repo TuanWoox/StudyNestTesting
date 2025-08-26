71b44f7a79dfc4daa584b5aacb0ad8f066b69a5e
Vu Nguyen <z1venttt@gmail.com>
2025-08-26 22:46:53 +0700

    Add React Query Client and React Devtools

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,12 +4,18 @@ import { router } from "./Router";
 import { Toaster } from 'sonner';
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import queryClient from "@/config/reactQueryConfig";
 
 function App() {
   return (
     <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster richColors expand={false} position="top-right" />
+      <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>
     </Provider>
   )
 }
