402acc9cbd73e44f53ab5f88337ab552c681f677
Vu Nguyen <z1venttt@gmail.com>
2025-10-07 13:33:40 +0700

    Add HangFire

---
+++ BackEnd/StudyNest/Infrastructures/Hangfire/HangfireConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+
+namespace StudyNest.Infrastructures.Hangfire
+{
+    public class HangfireConfiguration
+    {
+        public static void ConfigureGlobalFilters()
+        {
+            GlobalJobFilters.Filters.Add(new AutomaticRetryAttribute
+            {
+                Attempts = 5,
+                DelaysInSeconds = new[] { 1, 2, 5, 10, 30 },
+                OnAttemptsExceeded = AttemptsExceededAction.Delete
+            });
+        }
+    }
+}
