10c48abdb570ed773b1c9cc83196b6bbd29233dc
Nguyễn Tuấn Vũ <147223849+TuanWoox@users.noreply.github.com>
2025-10-13 21:28:24 +0700

    Scrum 64 only include necessary field in select note dto select tag dto select folder dto (#35)
    * Add DTO Support For Performance And Reading For SelectNote/Tag/FolderDTO
    
    * Add BaseEntity For Each DTO in Select

--- BackEnd/StudyNest.Common/Models/DTOs/EntityDTO/Folder/SelectFolderDTO.cs
+++ BackEnd/StudyNest.Common/Models/DTOs/EntityDTO/Folder/SelectFolderDTO.cs
@@ -1,16 +1,56 @@
 ﻿using AutoMapper;
+using StudyNest.Common.Attributes;
 using StudyNest.Common.DbEntities.BaseEntity;
 using StudyNest.Common.DbEntities.Entities;
-using StudyNest.Common.DbEntities.Identities;
-using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudyNest.Common.Models.DTOs.EntityDTO.Folder
 {
     [AutoMap(typeof(DbEntities.Entities.Folder), ReverseMap = true, PreserveReferences = true)]
-    public class SelectFolderDTO: BaseEntity<string>
+    public class SelectFolderDTO : BaseEntity<string>
     {
         public string FolderName { get; set; }
         public string OwnerId { get; set; }
-        public ICollection<DbEntities.Entities.Note> Notes { get; set; } = new List<DbEntities.Entities.Note>();
+        public ICollection<FolderNoteSummaryDTO> Notes { get; set; } = new List<FolderNoteSummaryDTO>();
+    }
+    // 4 types below support for select NOTE for better performance && reading
+
+    [AutoMap(typeof(DbEntities.Entities.Note), ReverseMap = true, PreserveReferences = true)]
+    public class FolderNoteSummaryDTO : BaseEntity<string>
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string Status { get; set; }
+        // Mapping to owner
+        public string OwnerId { get; set; }
+        //Mapping to Folder
+        public string FolderId { get; set; }
+        public NestedFolderSummaryDTO Folder { get; set; }
+        //Mapping to Tags
+        public ICollection<NoteTagRelationDTO> NoteTags { get; set; } = new List<NoteTagRelationDTO>();
+    }
+
+    [AutoMap(typeof(DbEntities.Entities.Folder), ReverseMap = true, PreserveReferences = true)]
+    public class NestedFolderSummaryDTO : BaseEntity<string>
+    {
+        public string FolderName { get; set; }
+        public string OwnerId { get; set; }
+    }
+
+    [AutoMap(typeof(NoteTag), ReverseMap = true, PreserveReferences = true)]
+    public class NoteTagRelationDTO : BaseEntity<string>
+    {
+        public string TagId { get; set; }
+        public TagSummaryDTO Tag { get; set; }
+    }
+
+    [AutoMap(typeof(DbEntities.Entities.Tag), ReverseMap = true, PreserveReferences = true)]
+    public class TagSummaryDTO : BaseEntity<string>
+    {
+        [TrimmedRequired]
+        public string Name { get; set; }
     }
 }
--- BackEnd/StudyNest.Common/Models/DTOs/EntityDTO/Tag/SelectTagDTO.cs
+++ BackEnd/StudyNest.Common/Models/DTOs/EntityDTO/Tag/SelectTagDTO.cs
@@ -2,9 +2,10 @@
 using StudyNest.Common.Attributes;
 using StudyNest.Common.DbEntities.BaseEntity;
 using StudyNest.Common.DbEntities.Entities;
-using StudyNest.Common.Models.DTOs.EntityDTO.NoteTagDTO;
+using StudyNest.Common.Models.DTOs.EntityDTO.Note;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,58 @@ using System.Threading.Tasks;
 namespace StudyNest.Common.Models.DTOs.EntityDTO.Tag
 {
     [AutoMap(typeof(DbEntities.Entities.Tag), ReverseMap = true, PreserveReferences = true)]
-    public class SelectTagDTO: BaseEntity<string>
+    public class SelectTagDTO : BaseEntity<string>
     {
         [TrimmedRequired]
         public string Name { get; set; }
-        //Mappping to Notes
-        public ICollection<SelectNoteTagDTO> NoteTags { get; set; } = new List<SelectNoteTagDTO>();
+        //Mapping to Notes
+        public ICollection<TagNoteRelationDTO> NoteTags { get; set; } = new List<TagNoteRelationDTO>();
+    }
+    // 5 types below support for select NOTE for better performance && reading
+
+    [AutoMap(typeof(NoteTag), ReverseMap = true, PreserveReferences = true)]
+    public class TagNoteRelationDTO : BaseEntity<string>
+    {
+        public string NoteId { get; set; }
+        public TagNoteSummaryDTO Note { get; set; }
+    }
+
+    [AutoMap(typeof(DbEntities.Entities.Note), ReverseMap = true, PreserveReferences = true)]
+    public class TagNoteSummaryDTO : BaseEntity<string>
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string Status { get; set; }
+        // Mapping to owner
+        public string OwnerId { get; set; }
+        //Mapping to Folder
+        public string FolderId { get; set; }
+        public NoteFolderSummaryDTO Folder { get; set; }
+        //Mapping to Tags
+        public ICollection<NestedNoteTagRelationDTO> NoteTags { get; set; } = new List<NestedNoteTagRelationDTO>();
+    }
+
+    [AutoMap(typeof(NoteTag), ReverseMap = true, PreserveReferences = true)]
+    public class NestedNoteTagRelationDTO : BaseEntity<string>
+    {
+        public string TagId { get; set; }
+        public NestedTagSummaryDTO Tag { get; set; }
+    }
+
+    [AutoMap(typeof(DbEntities.Entities.Tag), ReverseMap = true, PreserveReferences = true)]
+    public class NestedTagSummaryDTO : BaseEntity<string>
+    {
+        [TrimmedRequired]
+        public string Name { get; set; }
+    }
+
+    [AutoMap(typeof(DbEntities.Entities.Folder), ReverseMap = true, PreserveReferences = true)]
+    public class NoteFolderSummaryDTO : BaseEntity<string>
+    {
+        public string FolderName { get; set; }
+        public string OwnerId { get; set; }
     }
 }
